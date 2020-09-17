72e4f0ae9e468a8b25d1bdafea7ddc835bac9ad4
curtisrosenvall <curt.rosenvall11@gmail.com>
2020-09-17 16:25:49 -0600

    finished

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+// import React, {Component} from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// export default class App extends Component {
+//   constructor(){
+//     super();
+
+//     this.state = {
+//       foods: ["spaghetti", "CURTIS", "sushi", "bologna", "cheese"]
+//     };
+//   }
+
+//   render(){
+//     let foodsToDisplay = this.state.foods.map((element, index) => {
+//       return <h2 key={index}>{element}</h2>
+//     });
+
+//     return <div className="App">{foodsToDisplay}</div>
+    
+//   }
+// }
+
+import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor (){
+    super();
+
+    this.state = {
+      foods: ["Curtis", "Jared", "Diana", "Rebecca"]
+    };
+  }
+  render() {
+    let foodsToDisplay = this.state.foods.map((element, index) => {
+      return <h2 key={index}>{element}</h2>
+    });
+
+    return <div className="App">{foodsToDisplay}</div>
+    
+
+    
+  }
 }
-
-export default App;
